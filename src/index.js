ceed8c5f7d02e8234ac494833a000a6457e8219d
alvinashiatey <alvinashiatey@gmail.com>
2020-04-23 11:35:39 -0400

    adding to github repo

--- src/index.js
+++ src/index.js
@@ -2,32 +2,53 @@ import "./styles/main.scss";
 import * as p5 from "p5";
 
 let a = (aa) => {
-  let w = aa.windowWidth / 2;
-  let h = aa.windowHeight;
+  let colors = [
+    "#dddae8",
+    "#a4bcc2",
+    "#779fb5",
+    "#56b093",
+    "#96a480",
+    "#9ab9ad",
+    "#2a514d",
+    "#d9ea9a",
+    "#a09958",
+    "#c58b68",
+  ];
+  let qouteInsert = document.querySelector(".quote");
+  let quotes = [
+    "&#182; alone, i am nothing. i have nothing.we have power.but we will never know it,we will never see it work.unless we come together to make it work.",
+    "&#182; true, I used to see a lot of hope. I saw men tear down the veils behind which the truth had been hidden. But then the same men, when they have power in their hands at last, began to find the veils useful.",
+  ];
+  qouteInsert.innerHTML = `<p>${
+    quotes[Math.floor(Math.random() * quotes.length)]
+  }</p>`;
+
   aa.setup = () => {
+    let w = aa.windowWidth / 2;
+    let h = aa.windowHeight;
     aa.createCanvas(w, h);
-  };
-
-  aa.draw = () => {
-    aa.background("rgba(255,255,250,1.0)");
-
-    aa.stroke("#a97");
-    for (let i = 0; i < 300; i++) {
+    aa.background(colors[Math.floor(Math.random() * colors.length)]);
+    aa.stroke(colors[Math.floor(Math.random() * colors.length)]);
+    aa.strokeWeight(5);
+    for (let i = 0; i < 40; i++) {
       let noiseVal = aa.noise(Math.random() * h, Math.random() * w);
       aa.line(
-        Math.random() * h,
         Math.random() * w,
-        noiseVal,
-        i / Math.random(50)
+        Math.random() * h,
+        i / Math.random(50),
+        Math.random() * w
       );
       aa.point(Math.random() * w, Math.random() * h);
       aa.angleMode(aa.DEGREES);
-      aa.rotate(2);
+      aa.rotate(35 + i);
     }
   };
 
+  aa.draw = () => {};
+
   aa.windowResized = () => {
     aa.resizeCanvas(aa.windowWidth / 2, aa.windowHeight);
+    aa.setup();
   };
 };
 const P5 = new p5(a);
