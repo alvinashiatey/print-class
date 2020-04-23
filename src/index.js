578badcfdeb3eb8a5b244e0fc73f3a008685785f
alvinashiatey <alvinashiatey@gmail.com>
2020-04-23 13:58:50 -0400

    colors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import "./styles/main.scss";
 import * as p5 from "p5";
 
 let a = (aa) => {
-  let colors = [
+  let colors, quotes, quoteInsert, letterNum, randSelec, offSet;
+
+  colors = [
     "#dddae8",
     "#a4bcc2",
     "#779fb5",
@@ -14,14 +16,16 @@ let a = (aa) => {
     "#a09958",
     "#c58b68",
   ];
-  let qouteInsert = document.querySelector(".quote");
-  let quotes = [
+  quoteInsert = document.querySelector(".quote");
+  quotes = [
     "&#182; alone, i am nothing. i have nothing.we have power.but we will never know it,we will never see it work.unless we come together to make it work.",
     "&#182; true, I used to see a lot of hope. I saw men tear down the veils behind which the truth had been hidden. But then the same men, when they have power in their hands at last, began to find the veils useful.",
   ];
-  qouteInsert.innerHTML = `<p>${
-    quotes[Math.floor(Math.random() * quotes.length)]
-  }</p>`;
+
+  randSelec = Math.floor(Math.random() * quotes.length);
+  quoteInsert.innerHTML = `<p>${quotes[randSelec]}</p>`;
+  letterNum = quotes[randSelec].split(/\s/);
+  offSet = 16;
 
   aa.setup = () => {
     let w = aa.windowWidth / 2;
@@ -29,18 +33,24 @@ let a = (aa) => {
     aa.createCanvas(w, h);
     aa.background(colors[Math.floor(Math.random() * colors.length)]);
     aa.stroke(colors[Math.floor(Math.random() * colors.length)]);
+    // aa.strokeWeight(0);
+    // aa.fill(colors[Math.floor(Math.random() * colors.length)]);
+    // aa.rect(0, 700, w, 250);
     aa.strokeWeight(5);
-    for (let i = 0; i < 40; i++) {
+    for (let i = 0; i < letterNum.length; i++) {
+      let offsetX = aa.random(-offSet, offSet);
+      let offsetY = aa.random(-offSet, offSet);
       let noiseVal = aa.noise(Math.random() * h, Math.random() * w);
       aa.line(
-        Math.random() * w,
+        Math.random() * w + offsetX,
         Math.random() * h,
-        i / Math.random(50),
+        i / Math.random(letterNum.length) + offsetY,
         Math.random() * w
       );
       aa.point(Math.random() * w, Math.random() * h);
+      for (let j = 0; j < letterNum.length; j++) {}
       aa.angleMode(aa.DEGREES);
-      aa.rotate(35 + i);
+      aa.rotate(letterNum.length + i);
     }
   };
 
